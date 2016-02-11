5f582141c0a457daac53b73e045f435e95329b2c
Steeven Guerrero <Steeven Guerrero>
2016-02-10 20:25:36 -0500

    asdfghhj

---
+++ server.js
@@ -0,0 +1,52 @@
+'use strict'
+const express = require('express')
+const http = require('http')
+const bodyParser=require('body-parser')
+const Firebase= require('firebase')
+let items=[]
+let contador=0
+
+
+let miRuta= new Firebase('https://usuariosturist.firebaseio.com/')
+let router=express.Router()
+router.use(bodyParser())
+router.route('/')
+.get(function(request,response){
+    items=[]
+    miRuta.once("value",function(snap){
+        let nuevoCliente=snap.val()
+        items.push(nuevoCliente)
+        console.log(contador++)
+        return response.send(items)
+        //response.status(200).send(items)
+     })
+     //setTimeout(function(){
+     //    response.json(items)
+     //},1000)
+
+})
+.post(function(req,res,next){
+    miRuta.child(req.body.cedula).set(req.body)
+    res.status(200).send('Accion completada')
+})
+.put(function(req,res,next){
+
+    miRuta.child(req.body.cedula).set(req.body)
+     res.status(200).send(req.body.codigo)
+})
+.delete(function(req,res,next){
+    miRuta.child(req.body.cedula).remove(function(error){
+        if (error)
+         {
+             return res.status(404).send('error de todas')
+         }
+     })
+    return res.status(200).send('ok')
+});
+
+var port=Number (process.env.PORT || 3000);
+let app= express()
+.use('/todo',router)
+.use(express.static(__dirname+'/public'))
+.listen(port)
+
